8ac9ed90d8fcd045128654e8b90323e5f61aa4dd
Stefan Schweter <stefan@schweter.it>
2018-03-19 21:15:39 +0100

    capsnet: remove implementation and use git submodule instead

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from keras.datasets import imdb
 from keras.preprocessing import sequence
 from keras.models import Model
 
+from vendor.Capsule.Capsule_Keras import *
 
 gru_len = 256
 Routings = 3
@@ -16,95 +17,6 @@ rate_drop_dense = 0.28
 max_features = 20000
 maxlen = 1000
 embed_size = 256
-
-
-def squash(x, axis=-1):
-    s_squared_norm = K.sum(K.square(x), axis, keepdims=True)
-    scale = K.sqrt(s_squared_norm + K.epsilon())
-    return x / scale
-
-# Notice: the base implementation is taken from
-# https://www.kaggle.com/chongjiujjin/capsule-net-with-gru
-# Modifications and refactoring are in progress.
-class Capsule(Layer):
-    def __init__(
-            self,
-            num_capsule=10,
-            dim_capsule=16,
-            routings=3,
-            share_weights=True,
-            activation='default',
-            **kwargs):
-        super(Capsule, self).__init__(**kwargs)
-        self.num_capsule = num_capsule
-        self.dim_capsule = dim_capsule
-        self.routings = routings
-        self.share_weights = share_weights
-        if activation == 'default':
-            self.activation = squash
-        else:
-            self.activation = Activation(activation)
-
-    def build(self, input_shape):
-        super(Capsule, self).build(input_shape)
-        input_dim_capsule = input_shape[-1]
-        if self.share_weights:
-            self.W = self.add_weight(
-                name='capsule_kernel',
-                shape=(
-                    1,
-                    input_dim_capsule,
-                    self.num_capsule *
-                    self.dim_capsule),
-                initializer='he_uniform',
-                trainable=True)
-        else:
-            input_num_capsule = input_shape[-2]
-            self.W = self.add_weight(
-                name='capsule_kernel',
-                shape=(
-                    input_num_capsule,
-                    input_dim_capsule,
-                    self.num_capsule *
-                    self.dim_capsule),
-                initializer='he_uniform',
-                trainable=True)
-
-    def call(self, u_vecs):
-        if self.share_weights:
-            u_hat_vecs = K.conv1d(u_vecs, self.W)
-        else:
-            u_hat_vecs = K.local_conv1d(u_vecs, self.W, [1], [1])
-
-        batch_size = K.shape(u_vecs)[0]
-        input_num_capsule = K.shape(u_vecs)[1]
-        u_hat_vecs = K.reshape(
-            u_hat_vecs,
-            (batch_size,
-             input_num_capsule,
-             self.num_capsule,
-             self.dim_capsule))
-        u_hat_vecs = K.permute_dimensions(u_hat_vecs, (0, 2, 1, 3))
-        # final u_hat_vecs.shape = [None, num_capsule, input_num_capsule,
-        # dim_capsule]
-
-        # shape = [None, num_capsule, input_num_capsule]
-        b = K.zeros_like(u_hat_vecs[:, :, :, 0])
-        for i in range(self.routings):
-            # shape = [None, input_num_capsule, num_capsule]
-            b = K.permute_dimensions(b, (0, 2, 1))
-            c = K.softmax(b)
-            c = K.permute_dimensions(c, (0, 2, 1))
-            b = K.permute_dimensions(b, (0, 2, 1))
-            outputs = self.activation(K.batch_dot(c, u_hat_vecs, [2, 2]))
-            if i < self.routings - 1:
-                b = K.batch_dot(outputs, u_hat_vecs, [2, 3])
-
-        return outputs
-
-    def compute_output_shape(self, input_shape):
-        return (None, self.num_capsule, self.dim_capsule)
-
 
 def get_model():
     input1 = Input(shape=(maxlen,))
